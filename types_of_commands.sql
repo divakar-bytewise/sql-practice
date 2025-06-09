5b355d51a30f45f6747220f896164410ee931f70
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-09 18:38:32 +0530

    implemented DDL,DCL,TCL sql commands.

--- types_of_commands.sql
+++ types_of_commands.sql
@@ -1,10 +1,11 @@
-create database sql_divakar; --creating a database to work with mysql.
-
 select * from sys.databases; --command to see the databases present in the system.
 
 use sql_divakar; --If we want to work with our database we have to make use of 'use' command.
 
---creating the tables related to cricket and this the table for players.
+
+--DDL
+
+--1. (create) creating the tables related to cricket and this the table for players.
 create table players(player_id int primary key,player_name varchar(20),
 player_role varchar(20),dob date,nationality varchar(10));
 
@@ -27,70 +28,154 @@ foreign key (match_id) references matches(match_id));
 create table umpires(match_id int,umpire_id int primary key,umpire_name varchar(20),
 umpire_role varchar(20),foreign key (match_id) references matches(match_id));
 
---inserting values into players table.
-insert into players values(1, 'virat kohli', 'batsman', '1988-11-05', 'India'),
-(2, 'jasprit bumrah', 'bowler', '1993-12-06', 'India'),
-(3, 'ben stokes', 'all-rounder', '1991-06-04', 'England'),
-(4, 'joe root', 'batsman', '1990-12-30', 'England'),
-(5, 'rashid khan', 'bowler', '1998-09-20', 'Afghanistan');
+--creating index for player_name in players table.
+create index idx_player_name on players(player_name);
 
---inserting values into teams table.
-insert into teams values(1, 'India', 'rahul dravid'),
-(2, 'England', 'brendon mccullum'),
-(3, 'Australia', 'andrew mcdonald'),
-(4, 'Pakistan', 'grant bradburn'),
-(5, 'Afghanistan', 'jonathan trott');
 
---inserting values into matches table.
-insert into matches values(101, 1, 2, '2024-03-10', 'mumbai', 1),
-(102, 3, 4, '2024-03-12', 'sydney', 4),
-(103, 2, 5, '2024-03-14', 'london', 2),
-(104, 1, 3, '2024-03-16', 'delhi', 3),
-(105, 4, 5, '2024-03-18', 'lahore', 5);
 
---inserting values into players_stats table.
-insert into players_stats values(1, 101, 1, 75, 50, 0, 0),
-(2, 101, 2, 10, 8, 10, 2),
-(3, 102, 3, 45, 35, 6, 1),
-(4, 103, 5, 5, 10, 8, 3),
-(5, 105, 4, 80, 60, 0, 0);
+--2. (alter) i have altered the size of the nationality column in players table.
+alter table players alter column nationality varchar(20);
 
+--by using alter command adding the CHECK constraint to the players table.
+alter table players add constraint check_dob_range check(year(dob) between 1988 and 2010);
+
+--by using alter command adding the NOT NULL constraint to the players table.
+alter table players alter column player_name varchar(20) not null;
+
+--i am dropping the umpires table.
+alter table umpires drop column umpire_role;
+
+--checking whether the column is deleted or not.
+select * from umpires
+
+--adding the column to the umpires_table.
+alter table umpires add umpire_role varchar(40);
+
+--updating the values to the column created (umpire_role).
+update umpires set umpire_role='main' where umpire_id=1;
+update umpires set umpire_role='main' where umpire_id=2;
+update umpires set umpire_role='third' where umpire_id=3;
+update umpires set umpire_role='reserve' where umpire_id=4;
+update umpires set umpire_role='main' where umpire_id=5;
+
+--renaming the table name.
+exec sp_rename 'umpires', 'umpires_details';
+
+select * from umpires_details;
+
+-- drop a table
+drop table umpires_details;
+
+-- droping an index
+drop index idx_player_name on players;
+
+--deleting all the values in a table
+truncate table umpires_details;
 
 --inserting values into umpires table.
-insert into umpires values(101, 1, 'kumar dharmasena', 'main'),
+insert into umpires_details values(101, 1, 'kumar dharmasena', 'main'),
 (102, 2, 'marais erasmus', 'main'),
 (103, 3, 'aleem dar', 'third'),
 (104, 4, 'rod tucker', 'reserve'),
 (105, 5, 'paul reiffel', 'main');
 
---i have altered the size of the nationality column in players table.
-alter table players alter column nationality varchar(20);
 
---to display all the values in the table we make use of select *.
+--DCL
+
+--switching to master database.
+use master;
+
+--creating a user login and password.
+create login divakar with password = 'Diva07@.';
+
+--switching to sql_divakar database.
+use sql_divakar;
+
+--inside the sql_divakar database creating a user.
+create user divakar for login divakar;
+
+--granting access to the new user in the database.
+grant update on players to divakar;
+
+--switching to the new user.
+exec as user='divakar';
+
+--retrieving the players table data.
 select * from players;
-select * from teams;
-select * from matches;
-select * from players_stats;
-select * from umpires;
 
---by using alter command adding the CHECK constraint to the players table.
-alter table players add constraint check_dob_range check(year(dob) between 1988 and 2010);
+--dropping new user. 
+drop user divakar;
 
---the command used to see the tables in the database.
-select * from sys.tables;
+--switching to master database.
+use master;
 
---by using alter command adding the NOT NULL constraint to the players table.
-alter table players alter column player_name varchar(20) not null;
+--dropping new user login in master database.
+drop login divakar;
 
---creating index for player_name in players table.
-create index idx_player_name on players(player_name);
+--creating a new role.
+create role data_engineer;
 
---i am dropping the umpires table.
-drop table umpires;
+--granting the access .
+grant update on players to data_engineer;
 
---adding runs scored by all players.
-select sum(runs_scored) as total_runs from players_stats;
+--assigning the user to particular role.
+exec sp_addrolemember 'data_engineer', 'divakar';
 
---calculating avg - runs scored by the players. 
-select avg(runs_scored) as average_runs from players_stats;
+--command to view the current user name in the database. 
+select user_name();
+
+--command to view the current user name in the server. 
+select suser_name(); 
+
+--revoking the access from the new user.
+revoke update on players from divakar;
+
+
+--TCL
+
+--starting transaction.
+begin transaction;
+
+--inserting a new data into players.
+insert into players values(7,'msd','batsman-wk','1988-04-30','India');
+
+--retrieving the players table data.
+select * from players;
+
+--commiting changes permentaly.
+commit;
+
+--starting transaction.
+begin transaction;
+
+--deleting a row of data.
+delete from players where player_id=1;
+
+--retrieving the players table data.
+select * from players;
+
+--rollbacking the changes made.
+rollback;
+
+--starting transaction.
+begin transaction;
+
+--deleting a row of data.
+delete from players where player_id=7;
+
+--saving a changes in transaction.
+save transaction sp1;
+
+--retrieving the players table data.
+select * from players;
+
+--updating the player 1 nationality.
+update players set nationality='bharat' where player_id=1;
+
+--rollbacking the changes made.
+rollback transaction sp1;
+
+--commiting changes permentaly.
+commit;
+
 
