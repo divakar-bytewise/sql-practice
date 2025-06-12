27d5cf8a387145b00f3d66a88b802fbb81a37de0
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-12 19:28:40 +0530

    implemented windows functions like rank, row number, dense rank, ntile, lag and lead

---
+++ src/sql_windows_function.sql
@@ -0,0 +1,27 @@
+--using lag lead for previous and next details .
+select player_id, runs_scored,
+lag(runs_scored) over (order by player_id) as prev_score,
+lead(runs_scored) over (order by player_id) as next_score
+from players_stats;
+
+--command to see the schema of a table.
+exec sp_help 'players';
+
+--using the rank function gave rank for the players.
+select player_id,runs_scored, 
+rank() over (order by player_id) as rank from players_stats;
+
+--using ntile to divide the group by 3.
+select player_id, runs_scored,
+ntile(3) over (order by runs_scored desc) as performance_bucket
+from players_stats;
+
+--using dense rank function and gave rank for the players.
+select player_id, runs_scored,
+dense_rank() over (order by runs_scored desc) as dense_player_rank
+from players_stats;
+
+--using row_number to give numbers to the row.
+select player_id, runs_scored,
+row_number() over (order by runs_scored desc) as rank_by_runs
+from players_stats;
